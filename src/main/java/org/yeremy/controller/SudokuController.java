c69405f6425811d561a61f1a39d299ef843f3127
Yeremy Turcios <yeremyturcios@gmail.com>
2016-05-25 14:12:52 -0600

    Set up Springboot and gcloud maven plugin

--- src/main/java/org/yeremy/controller/SudokuController.java
+++ src/main/java/org/yeremy/controller/SudokuController.java
@@ -1,22 +1,38 @@
 package org.yeremy.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-@Controller
-@EnableAutoConfiguration
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class SudokuController
 {
     @RequestMapping("/")
-    @ResponseBody
-    String home() {
+    public String home()
+    {
         return "Hello World!";
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)
+    {
         SpringApplication.run(SudokuController.class, args);
     }
 }
